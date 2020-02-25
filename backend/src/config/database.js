1d95688e7d38a6d91a49585607f69e4448395327
Igorsteixeira94 <igorsteixeira94@gmail.com>
2020-02-24 23:45:10 -0300

    Feature: Migration/Model User

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  database: 'gobarber',
+  username: 'root',
+  password: '123',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
