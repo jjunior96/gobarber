c801ea33da7b99bd90a4f913c79e96bc4e86c111
Igorsteixeira94 <igorsteixeira94@gmail.com>
2020-02-29 22:23:26 -0300

    Feature: Provider Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import middlewareAuth from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(middlewareAuth);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
